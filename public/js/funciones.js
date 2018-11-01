cb4202a561e564cceffaef666d2fc72fbf3b3f09
DerickTotto <derick.carcamo@unah.hn>
2018-10-31 22:21:51 -0600

    Ramsey login, registro, landingPage, home, areadeTrabajo

---
+++ public/js/funciones.js
@@ -0,0 +1,9 @@
+
+console.log("funciones");
+function nuevo(){
+    console.log("funciones2");
+    document.getElementById("contenidohome").innerHTML +=
+                                                            `<section class="proyecto col-xl-2 mr-4 mb-4" style="background-color:#007bff;">
+                                                                <img src="" alt="">
+                                                            </section>`
+}
